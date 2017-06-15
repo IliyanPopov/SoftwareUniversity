cb034718a2e79952fa43f1249164921a47d90c31
iliyan <iliyan.popov93@gmail.com>
2017-06-15 15:50:39 +0300

    BashSoft project added Just completed part 5 of the exercises

---
+++ C#OOP/BashSoftProject/BashSoft/BashSoft/Startup.cs
@@ -0,0 +1,10 @@
+﻿namespace BashSoft
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            InputReader.StartReadingCommands();
+        }
+    }
+}
---
+++ C#OOP/BashSoftProject/BashSoft/BashSoft/StudentsRepository.cs
@@ -0,0 +1,130 @@
+﻿namespace BashSoft
+{
+    using System.Collections.Generic;
+    using System.IO;
+
+    public static class StudentsRepository
+    {
+        public static bool IsDataInitialized = false;
+
+        private static Dictionary<string, Dictionary<string, List<int>>> studentsByCourse;
+
+        public static void InitializeData(string fileName)
+        {
+            if (!IsDataInitialized)
+            {
+                OutputWriter.WriteMessageOnNewLine("Reading data...");
+                studentsByCourse = new Dictionary<string, Dictionary<string, List<int>>>();
+                ReadData(fileName);
+            }
+            else
+            {
+                OutputWriter.WriteMessageOnNewLine(ExceptionMessages.DataIsAlreadyInitialized);
+            }
+        }
+
+        private static void ReadData(string fileName)
+        {
+            string path = SessionData.CurrentPath + "\\" + fileName;
+
+            if (File.Exists(path))
+            {
+                string[] allInputLines = File.ReadAllLines(path);
+
+                for (int line = 0; line < allInputLines.Length; line++)
+                {
+                    if (!string.IsNullOrEmpty(allInputLines[line]))
+                    {
+                        string[] data = allInputLines[line].Split(' ');
+                        string course = data[0];
+                        string student = data[1];
+                        List<int> marksList = new List<int>();
+
+                        for (int i = 2; i < data.Length; i++)
+                        {
+                            marksList.Add(int.Parse(data[i]));
+                        }
+
+                        // Add the course and student if they don't exist
+                        if (!studentsByCourse.ContainsKey(course))
+                        {
+                            studentsByCourse.Add(course, new Dictionary<string, List<int>>());
+                        }
+
+                        if (!studentsByCourse[course].ContainsKey(student))
+                        {
+                            studentsByCourse[course].Add(student, new List<int>());
+                        }
+                        // Add the marks
+                        studentsByCourse[course][student].AddRange(marksList);
+
+                        IsDataInitialized = true;
+                        OutputWriter.WriteMessageOnNewLine("Data read!");
+                    }
+                }
+            }
+            else
+            {
+                OutputWriter.DisplayException(ExceptionMessages.InvalidPath);
+            }
+        }
+
+        private static bool IsQueryForCoursePossible(string courseName)
+        {
+            if (IsDataInitialized)
+            {
+                if (studentsByCourse.ContainsKey(courseName))
+                {
+                    return true;
+                }
+                else
+                {
+                    OutputWriter.DisplayException(ExceptionMessages.InexistingCourseInDataBase);
+                }
+            }
+            else
+            {
+                OutputWriter.DisplayException(ExceptionMessages.DataNotInitializedExceptionMessage);
+            }
+
+
+            return false;
+        }
+
+        private static bool IsQueryForStudentPossible(string courseName, string studentName)
+        {
+            if (IsQueryForCoursePossible(courseName) && studentsByCourse[courseName].ContainsKey(studentName))
+            {
+                return true;
+            }
+            else
+            {
+                OutputWriter.DisplayException(ExceptionMessages.InexistingStudentInDataBase);
+            }
+
+            return false;
+        }
+
+        public static void GetStudentScoresFromCouse(string courseName, string username)
+        {
+            if (IsQueryForStudentPossible(courseName, username))
+            {
+                OutputWriter.PrintStudent(
+                    new KeyValuePair<string, List<int>>(username, studentsByCourse[courseName][username]));
+            }
+        }
+
+        public static void GetAllStudentFromCourse(string courseName)
+        {
+            if (IsQueryForCoursePossible(courseName))
+            {
+                OutputWriter.WriteMessageOnNewLine($"{courseName}");
+
+                foreach (var student in studentsByCourse[courseName])
+                {
+                    OutputWriter.PrintStudent(student);
+                }
+            }
+        }
+    }
+}
